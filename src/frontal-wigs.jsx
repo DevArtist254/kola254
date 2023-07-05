a0cb637d1a7bdb0492924c18e392e3b1a1e67531
DevArtist254 <kevinwasonga415@outlook.com>
2023-07-05 04:38:29 +0300

    card-init

--- src/frontal-wigs.jsx
+++ src/frontal-wigs.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function FrontalWigs() {
   return (
-    <div>FrontalWigs</div>
+    <div className='main'>
+    <div className='content'>
+      <h1 className='dark head-main'>Frontal Wigs</h1>
+      <div className='cards'>
+       <div className='card'>
+        <div className='card__pri'>
+          <h2>Heading</h2>
+
+          <div className='card__textBox'>
+            <h3>Sub Heading</h3>
+            <div>Stars</div>
+          </div>
+        </div>
+        <div className='card__links'>
+          <p>Details</p>
+          <p>Add to cart</p>
+        </div>
+       </div> 
+      </div>
+    </div>
+    </div>
   )
 }
 
