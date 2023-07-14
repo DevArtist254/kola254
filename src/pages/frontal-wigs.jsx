6bd5099ca3fb9040d55116608cc203ee9aaf3da2
DevArtist254 <kevinwasonga415@outlook.com>
2023-07-14 03:05:20 +0300

    update fw

--- src/pages/frontal-wigs.jsx
+++ src/pages/frontal-wigs.jsx
@@ -3,11 +3,7 @@ import {loadFrontalWigsProductsAsync} from '../redux/products/product.actions';
 import {connect} from 'react-redux';
 import Cards from '../components/card-comp/cards.comp';
 
-function FrontalWigs({
-  loadFrontalWigsProducts,
-  frontalWigsProducts,
-  isloading,
-}) {
+function FrontalWigs({loadFrontalWigsProducts, frontalWigsProducts}) {
   useEffect(() => {
     loadFrontalWigsProducts();
   }, [loadFrontalWigsProducts]);
@@ -15,11 +11,7 @@ function FrontalWigs({
   function addToCart(e) {
     console.log(`Yes`);
   }
-  return isloading ? (
-    <div style={{width: '100%', height: '100vh'}}>
-      <h1>Loading data</h1>
-    </div>
-  ) : (
+  return (
     <div className='main'>
       <div className='content'>
         <h1 className='dark head-main'>Frontal Wigs</h1>
@@ -31,7 +23,6 @@ function FrontalWigs({
 
 const mapStateToProps = ({product}) => ({
   frontalWigsProducts: product.frontalWigsProducts,
-  isloading: product.isloading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
