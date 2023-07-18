61c30b0a6731d33852d8ef8e15b22f23142ded1f
DevArtist254 <kevinwasonga415@outlook.com>
2023-07-18 04:31:58 +0300

    signUp

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from 'react';
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
+import {auth} from '../firebase/firebase';
 
 function Login() {
+  const onSubmitWithGoogle = async (e) => {
+    e.preventDefault();
+
+    const provider = new GoogleAuthProvider();
+
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+
+        console.log(user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  const onSubmitWithFacebook = async (e) => {
+    e.preventDefault();
+
+    const provider = new FacebookAuthProvider();
+
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  };
   return (
-    <div>Login</div>
-  )
+    <div className='content'>
+      <form>
+        <button type='submit' onClick={onSubmitWithGoogle}>
+          Sign up with google
+        </button>
+        <button type='submit' onClick={onSubmitWithFacebook}>
+          Sign up with facebook
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
